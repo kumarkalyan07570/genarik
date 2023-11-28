db0a20d3754068a78591a0fe685c37e3e773ff9c
kumarkalyan07570 <kumarkalyan07570@gmail.com>
2023-11-28 23:42:58 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def kalyan(request):
+    return render(request,'kalayn.html')
+
+def sam(request):
+    return render(request,'sam.html')
